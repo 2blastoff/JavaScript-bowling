83ac4380e5e55dbd02c3b9127baa9a2ff56ab994
chris <chris@chris-MBP.home>
2016-02-27 14:52:51 +0000

    correct scoring push to obj

--- spec/bowlSpec.js
+++ spec/bowlSpec.js
@@ -5,44 +5,46 @@ describe ('bowl', function(){
 
 beforeEach (function() {
   bowl1 = new bowl();
-  num1 = bowl1._firstPinsKnockedDown();
-  num2 = bowl1._secondPinsKnockedDown();
-  diff = 10 - num1;
+  x = 2;
+  y = 3;
+  str = 10;
 });
 
-describe("#_firstPinsKnockedDown", function(){
-  it ('creates pin knockdown number inclusive 0 to 10', function() {
-    expect(bowl1._firstPinsKnockedDown()).toBeGreaterThan(-1);
-    expect(bowl1._firstPinsKnockedDown()).not.toBeGreaterThan(10);
-  });
-  });
-
-describe("#firstBowl", function (){
-  it ('pushes 0-9 to hash at index 0', function(){
-    // num = 2;
-    bowl1.firstBowl(num1);
-      expect(bowl1.obj).toEqual({[bowl1.frame]:[num, ]});
-    // });
-    // it ('pushes [10, x] to hash for strike', function(){
-      // str = 10;
-      bowl1.firstBowl(num1);
-        expect(bowl1.obj).toEqual({[bowl1.frame]:[10, 'x']});
-      });
-  });
+  describe("#_firstPinsKnockedDown", function(){
+    it ('creates pin knockdown number inclusive 0 to 10', function() {
+      expect(bowl1._firstPinsKnockedDown()).toBeGreaterThan(-1);
+      expect(bowl1._firstPinsKnockedDown()).not.toBeGreaterThan(10);
+    });
   });
 
-describe('#secondBowl', function(){
-  it('pushes 0-9 or spare(/) to hash at index 1 in obj', function(){
-//     // num2 = bowl1._secondPinsKnockedDown();
-    // num = 7;
-    // num1 = 2;
-    bowl1.firstBowl(num1);
-    bowl1.secondBowl(num2);
-//     if (diff - num2 === 0)
-    expect(bowl1.obj).toEqual({[bowl1.frame]:[num1, num2]});
-//     if (diff - num2 > 0)
-//     expect(bowl1.obj).toEqual({[bowl1.frame]:[num1, num2]});
+  describe('#_secondPinsKnockedDown', function(){
+    it('creates pin knockdown number inclusive 0 to residual pins', function(){
+      residual = 10 - bowl1._firstPinsKnockedDown;
+      expect(bowl1._secondPinsKnockedDown()).toBeGreaterThan(-1);
+      expect(bowl1._secondPinsKnockedDown()).not.toBeGreaterThan(residual);
+    });
   });
-  // });
 
+  describe("#firstBowl", function (){
+    it ('pushes 0-9 pin number to obj and creates frame number', function(){
+      bowl1.firstBowl(x);
+      expect(bowl1.obj[1]).toEqual([x]);
+    });
+    it ('pushes [10, x] to obj for strike', function(){
+      bowl1.firstBowl(str);
+      expect(bowl1.obj[1]).toEqual([10, 'x']);
+    });
+    it('increases frame number by 1 on each call', function(){
+      bowl1.firstBowl(x);
+      expect(bowl1.frame).toEqual(1);
+    });
+  });
+
+  describe('#secondBowl', function(){
+    it('pushes 0 to residual to obj at index 1', function(){
+      bowl1.firstBowl(x);
+      bowl1.secondBowl(y);
+      expect(bowl1.obj[1]).toEqual([x, y]);
+    });
+  });
 });
--- src/bowl.js
+++ src/bowl.js
@@ -1,6 +1,7 @@
 function bowl(){
   this.obj = {};
   this.frame = 0;
+  this.bowl = 0;
   this.residual = 10;
 }
 
@@ -8,24 +9,19 @@ bowl.prototype.firstBowl = function(firstpins){
   this.frame += 1;
   this.num = firstpins;
   this.residual -=  this.num;
-  // console.log(this.num);
-  // console.log(this.residual);
-  if (this.num > -1 && this.num < 10) { this.obj[this.frame] = [this.num, ]};
-  if (this.num === 10) {this.obj[this.frame]= [this.num, 'x'] };
-  // return (this.num > -1 && this.num < 10) ? this.obj[this.frame] = [this.num, ] : this.obj[this.frame]= [this.num, 'x'];
+  if (this.num > -1 && this.num < 10) { (this.obj[this.frame] = [this.num, ]); }
+  if (this.num === 10) { (this.obj[this.frame]= [this.num, 'x']); }
 };
 
 bowl.prototype.secondBowl = function(secondpins){
-  // if (this.residual === 0) return 'zero pins no second ball';
-  console.log(this.residual);
+  if (this.residual === 0) {
+  this.residual = 10;
+  return ('no second bowl');}
   this.num2 = secondpins;
-  if (this.residual === 0) { this.obj[this.frame].push('/'); }
-  if (this.residual > 0) { this.obj[this.frame].push(this.num2); }
-  // return (this.residual === 0) ? this.obj[this.frame].push('/') : this.obj[this.frame].push(this.num2);
+  if (this.residual - this.num2 === 0) { this.obj[this.frame].push('/'); }
+  if (this.residual - this.num2 > 0) { this.obj[this.frame].push(this.num2); }
+  this.residual = 10;
 };
-
-
-
 
 
 // private
@@ -34,5 +30,6 @@ bowl.prototype._firstPinsKnockedDown = function(){
 };
 
 bowl.prototype._secondPinsKnockedDown = function(){
-  return Math.floor(Math.random() * this.residual);
+  // var the = this.residual;
+  return Math.floor(Math.random() * this.residual + 1);
 };
