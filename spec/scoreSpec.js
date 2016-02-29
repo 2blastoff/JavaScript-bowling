8b4c473bbd9df802aef3099dade51e4d1fde346d
chris <chris@chris-MacBook-Pro.local>
2016-02-29 14:08:50 +0000

    on score class

--- spec/scoreSpec.js
+++ spec/scoreSpec.js
@@ -1,27 +1,22 @@
 describe ('score', function(){
 
 beforeEach (function() {
-  score1 = new score();
   obj = {1:[3,4], 2:[2,8], 3:[3,4], 4:[1,0], 5:[2,5]};
-
+  score1 = new score(obj);
 });
 
-  describe('#frameScore', function(){
-    it('returns the frame score when no strike or spare', function(){
-      expect(score1.frameScore(obj)).toEqual([7,10,7,1,7]);
+  describe('#makeMultiArray', function(){
+    it('sequencially sums frames when no strike or spare', function(){
+      expect(score1.newScore).toEqual([7,17,24,25,32]);
     });
   });
 
-  // describe('checks for spare or strike', function(){
+  // describe('#isSpareOrStrike checks for spare or strike', function(){
   //   it('checks for x or / in frame', function(){
   //
-  //     // expect(score1.isSpareOrStrike();)
-  //
+  //     expect(score1.isSpareOrStrike(obj));
   //   });
-
   // });
-
-
 
 
 
--- src/score.js
+++ src/score.js
@@ -1,20 +1,44 @@
 function score(){
+
+  this.newScore=[];
 }
 
-score.prototype.frameScore = function(obj){
-  var frameScores = [];
+score.prototype.frameScore = function(){
+  // var frameScores = [];
+  // for(var i = 1; i <= Object.keys(obj).length; i++){
+  //   var cube = obj[i][0] + obj[i][1];
+  //   frameScores.push(cube);
+  // }
+  // var summate = [0];
+  //   for (var x = 0; x < 5; x++){
+  //     var the = (frameScores[x] + summate[x]);
+  //     summate.push(the);
+  // }
+      // summate.shift(); console.log( summate);
+      console.log(this.summate);
+};
+
+score.prototype.makeMultiArray = function(obj){
+  // var frameScores = [];
   for(var i = 1; i <= Object.keys(obj).length; i++){
     var cube = obj[i][0] + obj[i][1];
-    frameScores.push(cube);
+    // frameScores.push(cube);
+    this.newScore.push(cube);
   }
 
-  var be = [];
-  // n = 0;
-    for (var x = 0; x <= 5; x++){
-      var the = (frameScores[x] + be[x-1]);
-        be.push(the);
-        // n += 1;
-}
-  console.log(be);
+score.prototype.makeMultiArray = function(obj){
+  // var frameScores = [];
+  for(var i = 1; i <= Object.keys(obj).length; i++){
+    var cube = obj[i][0] + obj[i][1];
+    // frameScores.push(cube);
+    this.newScore.push(cube);
+  }
+
+  this.summate = [0];
+    for (var x = 0; x < 5; x++){
+      var the = (frameScores[x] + this.summate[x]);
+      this.summate.push(the);
+  }
+      this.summate.shift(); return(this.summate);
 
 };
